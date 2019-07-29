b663924ff6c630d0dcdfc3a1a3275e183c36472c
Akash.Dhawale <Akash.Dhawale@effective.digital>
2019-07-29 12:35:26 +0530

    added comments to pattern example

--- pattern.js
+++ pattern.js
@@ -1,13 +1,26 @@
+/**
+ * Print pattern in below format
+ *
+ *  1
+ *  1 2
+ *  1 2 3
+ *  1 2 3 4
+ *  1 2 3 4 5
+ *
+ */
 var pattern = function () {
     var i = 1;
+    console.log("First approach - using 2 loops");
     for (i; i <= 5; i++) {
         var s_1 = '';
         var j = 1;
         for (j; j <= i; j++) {
-            s_1 = s_1 + '' + j;
+            s_1 = s_1 + ' ' + j;
         }
-        // console.log(s);
+        console.log(s_1);
     }
+    console.log('--------------------------');
+    console.log("Second approach - using only 1 loop and a counter");
     var count = 1;
     var k = 1;
     var s = '';
--- pattern.ts
+++ pattern.ts
@@ -1,15 +1,32 @@
+/**
+ * Print pattern in below format
+ * 
+ *  1
+ *  1 2
+ *  1 2 3
+ *  1 2 3 4
+ *  1 2 3 4 5
+ * 
+ */
+
 const pattern = () => {
     let i: number = 1;
+
+    console.log("First approach - using 2 loops");
 
     for (i; i <= 5; i++) {
         let s: string = '';
         let j: number = 1;
         for (j; j <= i; j++) {
-            s = s + '' + j;
+            s = s + ' ' + j;
         }
-        // console.log(s);
+        console.log(s);
     }
 
+    console.log('--------------------------');
+
+    console.log("Second approach - using only 1 loop and a counter");
+    
     let count:number = 1;
     let k:number = 1;
     let s: string = '';
